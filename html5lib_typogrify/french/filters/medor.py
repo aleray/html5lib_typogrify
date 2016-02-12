bf2347b8988673427ab98307b9696457370ed591
Alex Leray <alexandre@stdin.fr>
2016-02-12 21:56:32 +0100

    Handles properly (http|https|ftp):

--- html5lib_typogrify/french/filters/medor.py
+++ html5lib_typogrify/french/filters/medor.py
@@ -25,8 +25,12 @@ RE_RBRAKET_SUB = ur"\1".format(**CHARS)
 RE_COMMA_OR_PERIOD_MATCH = re.compile(ur"\s*([.,])".format(**CHARS))
 RE_COMMA_OR_PERIOD_SUB = ur"\1".format(**CHARS)
 
-RE_COLUMN_MATCH = re.compile(ur"\s*:".format(**CHARS))
-RE_COLUMN_SUB = ur"{NBSP}:".format(**CHARS)
+RE_COLUMN_MATCH = re.compile(ur"\s*(http|https|ftp)?:".format(**CHARS))
+def fix_columns(match):
+    if match.group(1):
+        return match.group()
+    else:
+        return ur"{NBSP}:".format(**CHARS)
 
 RE_PUNCT_MATCH = re.compile(ur"\s*([;!\?%])".format(**CHARS))
 RE_PUNCT_SUB = ur"{NBTHINSP}\1".format(**CHARS)
@@ -38,14 +42,12 @@ RE_RAQUO_MATCH = re.compile(ur"\s*{RAQUO}".format(**CHARS))
 RE_RAQUO_SUB = ur"{NBTHINSP}{RAQUO}".format(**CHARS)
 
 
-
-
 class Filter(_base.Filter):
     """
     >>> import html5lib
     >>> from html5lib.filters import sanitizer
     >>>
-    >>> string = u"Oui! Non  ? Regardez: un chien  ; un chat. 55 % 45%"
+    >>> string = u"Oui! Non  ? Regardez: un http://chien  ; un chat. 55 % 45%"
     >>> dom = html5lib.parse(string, treebuilder="dom")
     >>> walker = html5lib.getTreeWalker("dom")
     >>>
@@ -64,7 +66,7 @@ class Filter(_base.Filter):
             if type == "Characters":
                 text = token["data"]
 
-                text = RE_COLUMN_MATCH.sub(RE_COLUMN_SUB, text)
+                text = RE_COLUMN_MATCH.sub(fix_columns, text)
                 text = RE_COMMA_OR_PERIOD_MATCH.sub(RE_COMMA_OR_PERIOD_SUB, text)
                 text = RE_PUNCT_MATCH.sub(RE_PUNCT_SUB, text)
                 text = RE_LAQUO_MATCH.sub(RE_LAQUO_SUB, text)
